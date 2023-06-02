8193c0f2842ed0dbff4a430775174a5c6f551e42
chriscashen <chris.cashen@hotmail.com>
2023-06-02 11:15:38 -0700

    add signup form

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,57 +1,63 @@
-import React, { useState } from 'react'
-import styles from "./Login.module.css"
-import { Button, TextField, Typography } from '@mui/material'
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import { Button, TextField, Typography } from "@mui/material";
 
-interface Credentials {
+interface LoginCredentials {
   username: string;
   password: string;
 }
 
 export default function Login() {
-  const [credentials, setCredentials] = useState<Credentials>({ username: '', password: '' })
+  const [loginCredentials, setLoginCredentials] = useState<LoginCredentials>({
+    username: "",
+    password: "",
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setCredentials((prevCredentials) => ({
-      ...prevCredentials,
-      [name]: value
-    }))
+    setLoginCredentials((prevLoginCredentials) => ({
+      ...prevLoginCredentials,
+      [name]: value,
+    }));
   };
 
-  const handleLogin = ({username, password}: Credentials):void => {
-    console.log('Login', username, password )
-  }
+  const handleLogin = ({ username, password }: LoginCredentials): void => {
+    // HANDLE FIREBASE LOGIN REQUEST
+    console.log("Login", username, password);
+  };
 
-  const handleSubmit = (event: React.FormEvent):void => {
+  const handleSubmit = (event: React.FormEvent): void => {
     event.preventDefault();
-    handleLogin(credentials)
-    setCredentials({ username: '', password: '' })
-  }
+    handleLogin(loginCredentials);
+    setLoginCredentials({ username: "", password: "" });
+  };
   return (
-    <form className={styles['login-form']} onSubmit={handleSubmit}>
-      <Typography variant='h2'>Login</Typography>
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
+      <Typography variant="h2">Login</Typography>
       <TextField
         label="email"
-        type='email'
-        placeholder='chris@spendwise.com'
-        variant='outlined'
-        size='small'
+        type="email"
+        placeholder="chris@spendwise.com"
+        variant="outlined"
+        size="small"
         margin="dense"
-        name='username'
-        value={credentials.username}
+        name="username"
+        value={loginCredentials.username}
         onChange={handleChange}
       />
       <TextField
         label="password"
-        type='password'
-        variant='outlined'
-        size='small'
+        type="password"
+        variant="outlined"
+        size="small"
         margin="dense"
-        name='password'
-        value={credentials.password}
+        name="password"
+        value={loginCredentials.password}
         onChange={handleChange}
       />
-      <Button variant='contained' type="submit">Login</Button>
+      <Button variant="contained" type="submit">
+        Login
+      </Button>
     </form>
-  )
+  );
 }
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,8 +1,96 @@
-import React from 'react'
-import styles from "./Signup.module.css"
+import React, { useState } from "react";
+import styles from "./Signup.module.css";
+import { TextField, Button, Typography } from "@mui/material";
+
+interface SignupCredentials {
+  username: string;
+  confirmUsername: string;
+  password: string;
+  confirmPassword: string;
+}
 
 export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+  const [signupCredentials, setSignupCrendtials] = useState<SignupCredentials>(
+    { username: "", confirmUsername: "", password: "", confirmPassword: "" }
+  );
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = event.target;
+    setSignupCrendtials((prevCredentials) => ({
+      ...prevCredentials,
+      [name]: value
+    }))
+  }
+
+  const handleSignup = ({username, password}: SignupCredentials) => {
+    console.log(username, password)
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    const { username, confirmUsername, password, confirmPassword} = signupCredentials
+    if(username !== confirmUsername ) {
+      console.log("username does not match")
+      //set username does not match
+      return 
+    }
+
+    if(password !== confirmPassword ) {
+      console.log("password does not match")
+      //set username does not match
+      return 
+    }
+
+    handleSignup(signupCredentials)
+    setSignupCrendtials({ username: "", confirmUsername: "", password: "", confirmPassword: "" })
+
+  }
+
+  return <div><form className={styles["signup-form"]} onSubmit={handleSubmit}>
+  <Typography variant="h2">Signup</Typography>
+  <TextField
+    label="email"
+    type="email"
+    placeholder="chris@spendwise.com"
+    variant="outlined"
+    size="small"
+    margin="dense"
+    name="username"
+    value={signupCredentials.username}
+    onChange={handleChange}
+  />
+  <TextField
+    label="confirm email"
+    type="emial"
+    variant="outlined"
+    size="small"
+    margin="dense"
+    name="confirmUsername"
+    value={signupCredentials.confirmUsername}
+    onChange={handleChange}
+  />
+  <TextField
+    label="password"
+    type="password"
+    variant="outlined"
+    size="small"
+    margin="dense"
+    name="password"
+    value={signupCredentials.password}
+    onChange={handleChange}
+  />
+  <TextField
+    label="confirm password"
+    type="password"
+    variant="outlined"
+    size="small"
+    margin="dense"
+    name="confirmPassword"
+    value={signupCredentials.confirmPassword}
+    onChange={handleChange}
+  />
+  <Button variant="contained" type="submit">
+    Signup
+  </Button>
+</form></div>;
 }
