f715eecb5a7e2fc131bf7d4552f7d45e2f6f7c08
chriscashen <chris.cashen@hotmail.com>
2023-06-05 08:06:34 -0700

    complete signup

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,6 @@ import "firebase/firestore"
 import 'firebase/auth'
 
 const firebaseConfig = {
-    // apiKey: "AIzaSyD20eCEvYTnC3TxYiz80UPAAgoCUiZCcyM",
-    // authDomain: "spend-wise-d2b1a.firebaseapp.com",
-    // projectId: "spend-wise-d2b1a",
-    // storageBucket: "spend-wise-d2b1a.appspot.com",
-    // messagingSenderId: "40522040959",
-    // appId: "1:40522040959:web:c156bb9f70cdfcd86aa14a",
 apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
